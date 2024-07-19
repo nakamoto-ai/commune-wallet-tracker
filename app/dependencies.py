4f8196f160b78abc469252a5ba41c20f659da80b
Eddie Nakamoto <edwardnakamoto@icloud.com>
2024-07-19 10:55:31 -0500

    refactored dependencies.py imports

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,11 +1,10 @@
 from fastapi import Request
-from app.core.transactions.service import TransactionService
+
+from app.core.constants.commune import COMMUNE_BLOCKCHAIN_API_URL
+from app.core.constants.http import HEADER_CONTENT_TYPE, APPLICATION_JSON
 from app.core.graphql.client import GraphQLClient
 from app.core.graphql.query import GraphQLQueryLoader
-from app.core.graphql.client import GraphQLClient
-
-from app.core.constants.http import HEADER_CONTENT_TYPE, APPLICATION_JSON
-from app.core.constants.commune import COMMUNE_BLOCKCHAIN_API_URL
+from app.core.transactions.service import TransactionService
 
 graphql_client = GraphQLClient(
     url=COMMUNE_BLOCKCHAIN_API_URL,
